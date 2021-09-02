57cac205f4c0f68449e3d8f0701c0f6329f46e15
Aaron Galloway <aaron.j.galloway@gmail.com>
2021-09-02 01:50:52 -0400

    Update moonlight.sh
    Forgot this was the default entry point.

--- scripts/moonlight.sh
+++ scripts/moonlight.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -v $HOME/volumes/moonlight:/home/moonlight-user  --entrypoint /usr/local/bin/moonlight  moonlight/embedded $@
+docker run -v $HOME/volumes/moonlight:/home/moonlight-user moonlight/embedded $@
