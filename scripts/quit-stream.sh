ea03f54d1aab8defeb03f662b0bf701cfde97124
Aaron J Galloway <aaron.j.galloway@gmail.com>
2021-09-05 01:39:36 -0400

    Added --rm properly.

--- scripts/quit-stream.sh
+++ scripts/quit-stream.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 gfe_host="192.168.0.11"
-docker run -d -rm -v $HOME/volumes/moonlight:/home/moonlight-user  --entrypoint /usr/local/bin/moonlight moonlight/embedded quit $gfe_host
+docker run -d --rm -v $HOME/volumes/moonlight:/home/moonlight-user  --entrypoint /usr/local/bin/moonlight moonlight/embedded quit $gfe_host
