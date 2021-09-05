3a8aebb2f928062bf7aac87add921914fa0152fa
Aaron <aaron.j.galloway@gmail.com>
2021-09-05 00:49:25 -0400

    Modified scripts.

--- scripts/quit-stream.sh
+++ scripts/quit-stream.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 gfe_host="hostname"
-docker run -it  -v $HOME/volumes/moonlight:/home/moonlight-user  --entrypoint /usr/local/bin/moonlight moonlight/embedded quit $gfe_host
+docker run -d -v $HOME/volumes/moonlight:/home/moonlight-user  --entrypoint /usr/local/bin/moonlight moonlight/embedded quit $gfe_host
--- scripts/start-stream.sh
+++ scripts/start-stream.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 #host running GFE
-gfe_host="hostname"
-#add any other devices here
-#videocore and all CURRENTLY ACTIVE /dev/input/events by default
-#will try experimenting with -v /dev/input:/dev/input:ro or something later (experienced no video/audio output previously)
+gfe_host="192.168.0.11"
 
-devices="--device=/dev/vchiq `ls /dev/input |grep event |sed -e 's/^[[:space:]]*/--device=\/dev\/input\//'`"
+devices="--device=/dev/vchiq -v /dev/input:/dev/input -v /run/udev:/run/udev"
 
 dcmd="docker run -v $HOME/volumes/moonlight:/home/moonlight-user $devices moonlight/embedded"
 
 dargs="stream $gfe_host -app Desktop"
 
-eval $dcmd $dargs $@
+eval $dcmd $dargs $@ &
